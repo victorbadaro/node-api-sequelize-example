3c8cc76807703dc748a6bea37c6e30ac638ffcf6
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-26 18:31:26 -0300

    feat: add methods to find, create update and delete users

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,12 +1,21 @@
 const User = require('../models/User');
+const { Op } = require('sequelize');
 
 class UserController {
     async index(request, response) {
+        const users = await User.findAll();
 
+        return response.json(users);
     }
 
     async find(request, response) {
+        const { id } = request.params;
+        const user = await User.findByPk(Number(id));
 
+        if (!user)
+            return response.status(400).json({ error: 'User not found!' });
+
+        return response.json(user);
     }
 
     async create(request, response) {
@@ -22,11 +31,38 @@ class UserController {
     }
 
     async update(request, response) {
+        const { id } = request.params;
+        const { name, email } = request.body;
+        const user = await User.findByPk(Number(id));
 
+        if (!user)
+            return response.status(400).json({ error: 'User not found!' });
+
+        const userFoundByEmail = await User.findOne({
+            where: {
+                id: {
+                    [Op.ne]: id
+                },
+                email
+            }
+        });
+
+        if (userFoundByEmail)
+            return response.status(400).json({ error: 'Email already exists!' });
+
+        await User.update({ name, email }, { where: { id } });
+        return response.status(204).send();
     }
 
     async delete(request, response) {
+        const { id } = request.params;
+        const user = await User.findByPk(Number(id));
 
+        if (!user)
+            return response.status(400).json({ error: 'User not found!' });
+
+        await User.destroy({ where: { id } });
+        return response.status(204).send();
     }
 }
 
