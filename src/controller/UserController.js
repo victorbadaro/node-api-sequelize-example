d7fdcf75d365d7d43166656e2e25c7a075e5b7aa
Victor <victor.badaro@fatec.sp.gov.br>
2021-12-26 12:43:52 -0300

    feat: add user controller and its methods

---
+++ src/controller/UserController.js
@@ -0,0 +1,23 @@
+class UserController {
+    async index(request, response) {
+
+    }
+
+    async find(request, response) {
+
+    }
+
+    async create(request, response) {
+
+    }
+
+    async update(request, response) {
+
+    }
+
+    async delete(request, response) {
+
+    }
+}
+
+export default new UserController();
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import UserController from '../controller/UserController';
 
 const router = Router();
+
+router.get('/', UserController.index);
+router.post('/', UserController.create);
+router.get('/:id', UserController.find);
+router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 export { router };
